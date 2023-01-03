7873572e56652ab06e3f1d19cc00a63e4b67fa37
donghwan <ldh793@naver.com>
2023-01-04 02:08:48 +0900

    :sparkles: feat: 회원가입 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from .serializers import *
 
 # Create your views here.
 
@@ -12,3 +17,38 @@ from rest_framework.response import Response
 @api_view(['GET'])
 def test(request):
     return Response({'hello':'world'})
+
+'''
+작성자: 이동환
+설명: 회원가입
+'''
+@api_view(['POST'])
+def register(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+
+        # jwt
+        token = TokenObtainPairSerializer.get_token(user)
+        refresh_token = str(token)
+        access_token = str(token.access_token)
+        res = Response(
+            {
+                "user": serializer.data,
+                "token": {
+                    "access": access_token,
+                    "refresh": refresh_token,
+                },
+            },
+            status=status.HTTP_200_OK
+        )
+
+        # jwt 토큰 쿠키 저장(httponly => js로 조회 x)
+        res.set_cookie("access", access_token, httponly=True)
+        res.set_cookie("refresh", refresh_token, httponly=True)
+        return res
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+    return 
