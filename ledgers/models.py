3c74b557bc0b2d6071438f3ce78d96c0c83bcf30
donghwan <ldh793@naver.com>
2023-01-04 17:09:30 +0900

    :hammer: chore: 가계부 테이블 구현

--- ledgers/models.py
+++ ledgers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Ledger(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="ledgers")
+    memo = models.CharField(max_length=256, null=True)
+    cashflow = models.IntegerField()
+    year = models.IntegerField()
+    month = models.IntegerField()
+    day = models.IntegerField()
+    url = models.CharField(max_length=256, null=True)
+    
