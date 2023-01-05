89196ef4d26abbee4a3365345fe72361a8a11bca
donghwan <ldh793@naver.com>
2023-01-06 05:43:04 +0900

    :hammer: chore: name 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,8 @@ from rest_framework_simplejwt.views import TokenRefreshView
 app_name = "accounts"
 
 urlpatterns = [
-    path("signup/", views.signup),
-    path("login/", views.login),
-    path("logout/", views.logout),
+    path("signup/", views.signup, name="signup"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
     path("token/refresh/", TokenRefreshView.as_view()),
 ]
