0dcdce5e2e5a480c37fe9870b04e048f6ef85a77
donghwan <ldh793@naver.com>
2023-01-05 20:31:39 +0900

    :hammer: chore: 월,일 제한조건 추가

--- ledgers/models.py
+++ ledgers/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Ledger(models.Model):
@@ -7,8 +8,8 @@ class Ledger(models.Model):
     memo = models.CharField(max_length=256, null=True)
     cashflow = models.IntegerField()
     year = models.IntegerField()
-    month = models.IntegerField()
-    day = models.IntegerField()
+    month = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(12)])
+    day = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(31)])
     original_url = models.CharField(max_length=256, null=True)
     shorten_url = models.CharField(max_length=256, null=True)
     expiration_time = models.DateTimeField(null=True)
