c65b30aa77b4313ef1a32c96f47de7a58341adaf
donghwan <ldh793@naver.com>
2023-01-05 16:17:48 +0900

    :hammer: chore: app_name 추가

--- ledgers/urls.py
+++ ledgers/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "ledgers"
+
 urlpatterns = [
     path("<int:user_pk>/", views.ledger_list_or_create),
     path("<int:ledger_pk>/detail/", views.ledger_read_or_update_or_delete), 
