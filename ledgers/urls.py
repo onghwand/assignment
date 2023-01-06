df685c6e07d06cef94abeb3bc4c921a2e62d1c23
donghwan <ldh793@naver.com>
2023-01-06 16:41:49 +0900

    :hammer: chore: url name 추가

--- ledgers/urls.py
+++ ledgers/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = "ledgers"
 
 urlpatterns = [
-    path("<int:user_pk>/", views.ledger_list_or_create),
-    path("<int:ledger_pk>/detail/", views.ledger_read_or_update_or_delete), 
-    path("<int:ledger_pk>/duplicate/", views.ledger_duplicate),
-    path("<int:ledger_pk>/url/", views.make_shorten_url),
+    path("<int:user_pk>/", views.ledger_list_or_create, name="list_or_create"),
+    path("<int:ledger_pk>/detail/", views.ledger_read_or_update_or_delete, name="read_or_update_or_delete"), 
+    path("<int:ledger_pk>/duplicate/", views.ledger_duplicate, name="duplicate"),
+    path("<int:ledger_pk>/url/", views.make_shorten_url, name="shorten_url"),
 ]
