db30fbc22fea6a0a7ff504f848c4fbd6494bd396
donghwan <ldh793@naver.com>
2023-01-06 17:05:36 +0900

    :hammer: chore: swagger 추가

--- assignment/urls.py
+++ assignment/urls.py
@@ -17,7 +17,26 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+# swagger
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Ledger REST_API",
+      default_version='v1',
+       # 아래는 선택 인자
+      description="Payhere assignment REST_API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   #permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
+    path('', schema_view.with_ui('swagger')),
     path('admin/', admin.site.urls),
     path('api/v1/accounts/', include('accounts.urls')),
     path('api/v1/ledgers/', include('ledgers.urls')),
